5d25ecdc7951d7be9b773d9939a2c1fde4215642
Kevin Glover <hkglover@gmail.com>
2015-01-07 09:36:38 -0500

    Route specific classes
    - Added route specific classes to the body through ember

--- atlmapsEmber/js/app.js
+++ atlmapsEmber/js/app.js
@@ -134,6 +134,23 @@ App.LoginController  = Ember.Controller.extend(SimpleAuth.LoginControllerMixin, 
 
 // Routes
 
+Ember.Route.reopen({
+  activate: function() {
+    var cssClass = this.toCssClass();
+    // you probably don't need the application class
+    // to be added to the body
+    if (cssClass != 'application') {
+      Ember.$('body').addClass(cssClass);
+    }
+  },
+  deactivate: function() {
+    Ember.$('body').removeClass(this.toCssClass());
+  },
+  toCssClass: function() {
+    return this.routeName.replace(/\./g, '-').dasherize();
+  }
+});
+
 App.ApplicationRoute = Ember.Route.extend(SimpleAuth.ApplicationRouteMixin);
 
 App.IndexRoute = Ember.Route.extend({});
