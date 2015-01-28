a80d8079a289ac2385de2bcbcb4445fef5d12e19
Jay Varner <jayvarner@gmail.com>
2015-01-28 11:44:36 -0800

    Cleanup before delivering stories.

--- atlmaps/config/application.rb
+++ atlmaps/config/application.rb
@@ -23,13 +23,13 @@ module Atlmaps
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
+
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins 'atlmaps-dev.com'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
+        origins 'atlmaps-dev.org'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
-    
+
   end
 end
