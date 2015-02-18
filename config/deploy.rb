772e86913cbe77bf39c7c619e815aa53aab64de1
Jay Varner <jayvarner@gmail.com>
2015-02-18 13:21:33 -0800

    Typo fix :(

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :scm, :git
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/initializers/devise.rb', 'config/secrets.yml', 'config/applications.rb')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/initializers/devise.rb', 'config/secrets.yml', 'config/application.rb')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
