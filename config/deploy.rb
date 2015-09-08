04432ec010b99919b58c846302f30474572a1129
Jay Varner <jayvarner@gmail.com>
2015-09-08 15:51:02 -0400

    Fixed deploy path and added GS_Harvest task to ignore list.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:emory-libraries-ecds/ATLMaps.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/data/rails/atlmaps'
+set :deploy_to, '/data/atlmaps-server'
 
 # Default value for :scm is :git
 set :scm, :git
