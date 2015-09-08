3c68a49a377c2c14474e3dba86367c5eda3291cc
Jay Varner <jayvarner@gmail.com>
2015-09-08 15:54:38 -0400

    Removed commented code.

--- app/controllers/api/v1/vector_layers_controller.rb
+++ app/controllers/api/v1/vector_layers_controller.rb
@@ -10,33 +10,26 @@ class Api::V1::VectorLayersController < ApplicationController
     end
 
   end
-  
+
   def show
-    # if params[:project_id]
-    #   foo = Projectlayer.where(layer_id: params[:id]).where( project_id: params[:project_id]).limit(1)
-    #   foo.each do |marker|
-    #     @marker = marker.marker
-    #   end
-    #   @layer = Layer.find(params[:id])
-    # else
       layer = VectorLayer.find(params[:id])
     #end
-    render json: layer, root: 'vector_layer' 
+    render json: layer, root: 'vector_layer'
     #respond_to do |format|
     #  format.json { render json: layer, status: :ok }
     #end
   end
-  
+
   def update
     project = Project.find(params[:id])
     if project.update_attributes(params[:name])
       head 204, location: project
     end
   end
-  
+
   #private
   #  def project_params
   #    params.permit(:project_ids => [])
   #  end
-  
+
 end
