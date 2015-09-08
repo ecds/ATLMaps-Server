6ef981a06fe9f3a204cf185e63e226459425c1cd
Jay Varner <jayvarner@gmail.com>
2015-09-08 11:19:07 -0400

    Secured returning unpublished projects and projects can only be deleted by owner.

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,103 +1,90 @@
-class Api::V1::ProjectsController < ApplicationController
+# Controller for rendering projects.
+module Api
+  module V1
+    # foo
+    class ProjectsController < ApplicationController
+      # class for Controller
+      def index
+        if params[:name]
+          projects = Project.where(name: params[:name])
+        elsif params[:user_id]
+          projects = Project.where(user_id: params[:user_id])
+        elsif params[:collaborations]
+          projects = Project.joins(:collaboration).where(\
+            collaborations: { user_id: current_resource_owner.id })
+        else
+          projects = Project.where(published: true)
+        end
 
-  def index
-    if params[:name]
-      projects = Project.where(name: params[:name])
-    elsif params[:user_id]
-      projects = Project.where(user_id: params[:user_id])
-    elsif params[:collaborations]
-      projects = Project.joins(:collaboration).where(collaborations: {user_id: current_resource_owner.id})
-    else
-      projects = Project.where(published: true)
-    end
+        render json: projects, root: 'projects', resource_owner: owner_id
+      end
 
-    render json: projects, root: 'projects', resource_owner: owner_id
-    #respond_to do |format|
-    #  format.json { render json: projects, status: :ok }
-    #end
-  end
+      def show
+        # Only return the project if it is published, the user is the owner
+        # or the user is a collaborator.
+        @project = Project.find(params[:id])
+        if @project.published == true || is_mine == true || is_collaborator == true
+          render json: @project, root: 'project', resource_owner: owner_id
+        else
+          render json: {}, status: 401
+        end
+      end
 
-  def show
-    #if params[:id]
-    @project = Project.find(params[:id])
-    #elsif params[:slug]
-      #@project = Project.where(name: params[:id])
-    #end
+      def create
+        project = Project.new(project_params)
+        if project.save
+          head 204
+        end
+      end
 
-    # if current_resource_owner
-    #   @may_edit = is_collaborator
+      def update
+        project = Project.find(params[:id])
+        if project.update(project_params)
+          head 204
+        end
+      end
 
-    #   @is_mine = is_mine
+      def destroy
+        project = Project.find(params[:id])
+        if project.user_id == owner_id
+          project.destroy
+          head 204
+        else
+          render json: {}, status: 401
+        end
+      end
 
-    # else
-    #   if @project.owner == 'Guest'
-    #     @may_edit = true
-    #   end
-    # end
-    render json: @project, root: 'project', resource_owner: owner_id
+      private
 
-  end
+      def project_params
+        params.require(:project).permit(:name, :saved, :description, :center_lat, :center_lng, :zoom_level, :default_base_map, :user_id, :published)
+      end
 
-  def create
-    project = Project.new(project_params)
-    if project.save
-      head 204
-    end
-  end
+      def owner_id
+        current_resource_owner ? current_resource_owner.id : 0
+      end
 
-  def update
-    project = Project.find(params[:id])
-    if project.update(project_params)
-      head 204
-    end
-  end
+      def is_mine
+        if current_resource_owner && current_resource_owner.id == @project.user_id
+          return true
+        else
+          return false
+        end
+      end
 
-  def destroy
-    project = Project.find(params[:id])
-    project.destroy
-    head 204
-  end
-
-  private
-
-    def project_params
-      params.require(:project).permit(:name, :saved, :description, :center_lat, :center_lng, :zoom_level, :default_base_map, :user_id, :published)
-    end
-
-    def owner_id
-      # if current_resource_owner
-      #   return current_resource_owner.id
-      # else
-      #   return 0
-      # end
-      return current_resource_owner ? current_resource_owner.id : 0
-    end
-
-    def is_mine
-      if current_resource_owner && current_resource_owner.id == @project.user_id
-        return true
-      else
-        return false
+      def is_collaborator
+        collaborations = []
+        @project.collaboration.each do |collaborator|
+          collaborations << collaborator.user_id
+        end
+        if collaborations.any? and collaborations.include? current_resource_owner.id
+          return true
+        elsif is_mine
+          return true
+        else
+          return false
+        end
       end
     end
-
-    def is_collaborator
-      collaborations = []
-      @project.collaboration.each do |collaborator|
-        collaborations << collaborator.user_id
-      end
-      if collaborations.any? and collaborations.include? current_resource_owner.id
-        return true
-      elsif is_mine
-        return true
-      else
-        return false
-      end
-    end
-          
-
-    # def current_resource_owner
-    #   User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-    # end
-
+  end
 end
