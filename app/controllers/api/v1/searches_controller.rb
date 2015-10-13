ccbf7a0cf12b9d292eb45b9fc3da0724b0a551cc
Alex Zotov <alexandr.zotov@emory.edu>
2015-10-13 15:50:07 -0400

    edited controller to correspond to ember models

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -3,16 +3,16 @@ class Api::V1::SearchesController < ApplicationController
   respond_to :json, :html, :xml 
   
   def index
-  	if request.post?
+  	# if request.post?
 	    @layers = RasterLayer.all.by_institution( params[:name] )
 	    						.by_tags( params[:tags] ).by_date( params[:start_date], params[:end_date])
 	    @vectors = VectorLayer.all.by_institution( params[:name])
 	    						.by_tags( params[:tags] ).by_date( params[:start_date], params[:end_date])
-	end
-	@response = { :layers => @layers, :vectors => @vectors }
+	# end
+	@response = { :searches => @layers, :searches => @vectors }
 
 			
-	render json: @response, root: 'searches'
+	render json: @response, each_serializer: SearchSerializer, root: false
   end
   
   
