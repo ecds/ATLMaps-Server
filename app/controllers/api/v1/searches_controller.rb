a9ddb7b416620c324f1f5cb2179f2a1bdf0aca65
Jay Varner <jayvarner@gmail.com>
2015-10-14 19:49:32 -0400

    Tweaked the json response for the filters a little to just be a bare array of unique ids.

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -2,15 +2,19 @@ class Api::V1::SearchesController < ApplicationController
 
 
   def index
-  	# if request.post?
-	    @layers = RasterLayer.all.by_institution( params[:name] )
-	    						.by_tags( params[:tags] ).by_date( params[:start_date], params[:end_date])
-	    @vectors = VectorLayer.all.by_institution( params[:name])
-	    						.by_tags( params[:tags] ).by_date( params[:start_date], params[:end_date])
-	# end
-	@searches = {:searches => {:raster_layer_ids => @layers.as_json(only: [:id]), :vector_layer_ids => @vectors.as_json(only: [:id])}}
-
-
+    @rasters = RasterLayer.all.by_institution( params[:name] )
+    						.by_tags( params[:tags] ).by_date( params[:start_date], params[:end_date])
+    @vectors = VectorLayer.all.by_institution( params[:name])
+    						.by_tags( params[:tags] ).by_date( params[:start_date], params[:end_date])
+  # The goal is to provide an array of ids for vector and raster layers that
+  # match the filter. Sometimes a layer might satisify multiple filters, hence
+  # the `.uniq`.
+	@searches = {
+    :searches => {
+      :raster_layer_ids => @rasters.ids.uniq,
+      :vector_layer_ids => @vectors.ids.uniq
+    }
+  }
 
   render json: @searches
   end
