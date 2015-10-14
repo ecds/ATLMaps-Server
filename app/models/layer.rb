5aa3e8edfcc93a33f4270b828d184031054338d8
Jay Varner <jayvarner@gmail.com>
2015-10-14 19:31:12 -0400

    Removed  from the layer model.

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -1,13 +1,13 @@
 class Layer < ActiveRecord::Base
-  include Filtering
+  # include Filtering
   has_many :projectlayer
   has_many :projects, through: :projectlayer, dependent: :destroy
   belongs_to :institution
-  
+
   has_and_belongs_to_many :tags, dependent: :destroy
 
   #default_scope {includes(:projectlayer)}
-  
+
   # Attribute to use for html classes
   def tag_slugs
     return self.tags.map {|tag| tag.name.parameterize}.join(" ")
