e989f8a1c69ce49e11cdd2481474c30614b90a00
Alex Zotov <alexandr.zotov@emory.edu>
2015-10-16 10:57:20 -0400

    if condition params

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -7,19 +7,19 @@ class Api::V1::SearchesController < ApplicationController
     						.by_tags( params[:tags] ).by_date( params[:start_date], params[:end_date])
  
 
- if(params.has_key?(:tags) && params.has_key?(:start_date) && params.has_key?(:finish_date) && params.has_key?(:name))
- 	@rasters = []
- 	@vectors = []
- end 
+	 if(!params.has_key?(:tags) && !params.has_key?(:start_date) && !params.has_key?(:finish_date) && !params.has_key?(:name))
+	 	@rasters = []
+	 	@vectors = []
+	 end 
   # The goal is to provide an array of ids for vector and raster layers that
   # match the filter. Sometimes a layer might satisify multiple filters, hence
   # the `.uniq`.
 	@searches = {
     :searches => {
       # This feels hackey but Ember Data requires the object to have an ID
-      :id => 1,
-      :raster_layer_ids => @rasters.ids.uniq,
-      :vector_layer_ids => @vectors.ids.uniq
+	      :id => 1,
+	      :raster_layer_ids => (@rasters.ids.uniq if @rasters.any?),
+	      :vector_layer_ids => (@vectors.ids.uniq if @vectors.any?)
     }
   }
 
