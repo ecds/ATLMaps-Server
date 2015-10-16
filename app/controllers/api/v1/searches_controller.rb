52b94f48f64c7812bca5cef620dd1f3228e86bff
Jay Varner <jayvarner@gmail.com>
2015-10-16 10:14:13 -0400

    Return empty response when search parameters are empty.

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -2,29 +2,27 @@ class Api::V1::SearchesController < ApplicationController
 
   def index
     @rasters = RasterLayer.all.by_institution( params[:name] )
-    						.by_tags( params[:tags] ).by_date( params[:start_date], params[:end_date])
+                .by_tags( params[:tags] ).by_date( params[:start_date], params[:end_date])
     @vectors = VectorLayer.all.by_institution( params[:name])
-    						.by_tags( params[:tags] ).by_date( params[:start_date], params[:end_date])
- 
+                .by_tags( params[:tags] ).by_date( params[:start_date], params[:end_date])
 
- if(params.has_key?(:tags) && params.has_key?(:start_date) && params.has_key?(:finish_date) && params.has_key?(:name))
- 	@rasters = []
- 	@vectors = []
- end 
-  # The goal is to provide an array of ids for vector and raster layers that
-  # match the filter. Sometimes a layer might satisify multiple filters, hence
-  # the `.uniq`.
-	@searches = {
-    :searches => {
-      # This feels hackey but Ember Data requires the object to have an ID
-      :id => 1,
-      :raster_layer_ids => @rasters.ids.uniq,
-      :vector_layer_ids => @vectors.ids.uniq
-    }
-  }
-
-  render json: @searches
+    if(!params.has_key?(:tags) && !params.has_key?(:start_date) && !params.has_key?(:finish_date) && !params.has_key?(:name))
+        @rasters = []
+        @vectors = []
+        render json: 'Success'
+    else
+        # The goal is to provide an array of ids for vector and raster layers that
+        # match the filter. Sometimes a layer might satisify multiple filters, hence
+        # the `.uniq`.
+        @searches = {
+            :searches => {
+                # This feels hackey but Ember Data requires the object to have an ID
+                :id => 1,
+                :raster_layer_ids => @rasters.ids.uniq,
+                :vector_layer_ids => @vectors.ids.uniq
+            }
+        }
+        render json: @searches
+    end
   end
-
-
 end
