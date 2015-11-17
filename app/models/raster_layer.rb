6b240559e27a0c54ebca391265c364e117ee92f5
Alex Zotov <alexandr.zotov@emory.edu>
2015-11-17 09:39:00 -0500

    bug fix models raster layer

--- app/models/raster_layer.rb
+++ app/models/raster_layer.rb
@@ -6,8 +6,8 @@ class RasterLayer < ActiveRecord::Base
 
   has_and_belongs_to_many :tags, dependent: :destroy
 
-  scope :by_institution, -> (name) { joins(:institution).where(institutions: {name: name}) if name.present?}
-  scope :by_tags, -> (tags) { joins(:tags).where(tags: {name: tags}) if tags.present?}
+  scope :by_institution, -> name { joins(:institution).where(institutions: {name: name}) if name.present?}
+  scope :by_tags, -> tags { joins(:tags).where(tags: {name: tags}) if tags.present?}
   scope :by_date, -> (start_date,end_date) { where(date: start_date..end_date) if start_date.present? and end_date.present?}
 
   include PgSearch
