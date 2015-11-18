e172cb02269c8d07afe304b4cfc0cdbd5a00e74a
Alex Zotov <alexandr.zotov@emory.edu>
2015-11-18 13:42:16 -0500

    template_id fixes

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -74,8 +74,7 @@ module Api
       def project_params
         params.require(:project).permit(
           :name, :saved, :description, :center_lat, :center_lng, :zoom_level,
-          :default_base_map, :user_id, :published, :featured, :intro, :media) 
-          # :template_id)
+          :default_base_map, :user_id, :published, :featured, :intro, :media, :template_id)
       end
 
       def owner_id
