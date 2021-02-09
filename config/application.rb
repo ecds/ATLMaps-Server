7543f09103dcd4420a52b3799995a7b2813ac08a
Jay Varner <jayvarner@gmail.com>
2021-02-09 12:46:19 -0500

    Add application config file

---
+++ config/application.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative('boot')
+
+require('rails/all')
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Atlmaps
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults(5.0)
+    config.action_controller.include_all_helpers = false
+    config.middleware.use(ActionDispatch::Cookies)
+    config.middleware.use(ActionDispatch::Session::CookieStore)
+    config.api_only = true
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
