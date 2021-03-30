118fe5d1d6115823d7088f0a51079264ffd27bca
Jay Varner <jayvarner@gmail.com>
2021-03-30 10:17:39 -0400

    Update to Rails 6.1

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ source('https://rubygems.org')
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem('bootsnap')
 # RAILS!
-gem('rails', '~> 6.0')
+gem('rails', '~> 6.1')
 # Use postgres with the postgis plugin as the database for Active Record
 gem('activerecord-postgis-adapter', '~> 6.0.0')
 # Postgres
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
+  config.active_support.disallowed_deprecation = :raise
+  config.active_support.disallowed_deprecation_warnings = []
+
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
